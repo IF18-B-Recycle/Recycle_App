04e81e1625e9fbbcffeb822a139adc0db5f4d241
AwalRafi <Manukemprit25@gmail.com>
2020-12-15 17:48:10 +0700

    Membuat Sign Up dan login terhubung ke Autenticate dan database Firebase Android

--- app/src/main/java/com/example/recycle_app/MainActivity.java
+++ app/src/main/java/com/example/recycle_app/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.recycle_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogout = findViewById(R.id.logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intToMain);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/recycle_app/RegisterActivity.java
+++ app/src/main/java/com/example/recycle_app/RegisterActivity.java
@@ -1,14 +1,107 @@
 package com.example.recycle_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.example.recycle_app.Model.Pengguna;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText etFirstName, etLastName, etEmail, etPassword;
+    Button BtnRegister;
+    TextView tvSignIn;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        BtnRegister = findViewById(R.id.btRegister);
+        BtnRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btRegister:
+                BtnRegister();
+                break;
+
+        }
+    }
+
+    private void BtnRegister() {
+        final String firstname = etFirstName.getText().toString().trim();
+        final String lastname = etLastName.getText().toString().trim();
+        final String email = etEmail.getText().toString().trim();
+        final String pwd = etPassword.getText().toString().trim();
+
+
+        if (email.isEmpty()) {
+            etEmail.setError("Please enter email id");
+            etEmail.requestFocus();
+            return;
+        }
+
+        if (pwd.isEmpty()) {
+            etPassword.setError("Please enter Password");
+            etPassword.requestFocus();
+            return;
+        }
+
+        if (firstname.isEmpty()) {
+            etFirstName.setError("Please enter Nama");
+            etFirstName.requestFocus();
+            return;
+        }
+        if (lastname.isEmpty()) {
+            etLastName.setError("Please enter Nama");
+            etLastName.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmail.setError("Email anda tidak ditemukan");
+            return;
+        }
+        mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Pengguna user = new Pengguna(firstname, lastname, email, pwd);
+                    FirebaseDatabase.getInstance().getReference("Pengguna").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(RegisterActivity.this, "SignUp successful", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "SignUp Unsuccessful, Please Try Again", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
 }
