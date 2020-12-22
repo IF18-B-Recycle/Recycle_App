584ac5d58350b8a121f1061d6d5f746b23b5e292
Lukyrafi13 <63963416+Lukyrafi13@users.noreply.github.com>
2020-12-22 14:55:38 +0700

    Menambahkan activity__about__luky_rafi_anuggilarso

---
+++ app/src/main/java/com/example/recycle_app/About/Activity_About_LukyRafiAnuggilarso.java
@@ -0,0 +1,16 @@
+package com.example.recycle_app.About;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.recycle_app.R;
+
+public class Activity_About_LukyRafiAnuggilarso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__about__luky_rafi_anuggilarso);
+    }
+}
