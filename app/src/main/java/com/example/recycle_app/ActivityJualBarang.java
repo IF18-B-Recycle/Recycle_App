9f438a269cec006391a83a7758d4be42c13d3713
GalihPrasetya-kun <galih.prasta007@gmail.com>
2021-01-12 15:06:03 +0700

    Menyimpan data transaksi (ActivityJualBarang) kedalam firebase

--- app/src/main/java/com/example/recycle_app/ActivityJualBarang.java
+++ app/src/main/java/com/example/recycle_app/ActivityJualBarang.java
@@ -1,14 +1,168 @@
 package com.example.recycle_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.recycle_app.Model.ModelJualBarang;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class ActivityJualBarang extends AppCompatActivity {
+
+    TextView tvHargaKertas, tvHargaPlastik, tvHargaLogam, tvHargaKaca, tvHargaAlmu, tvHargaKardus;
+    EditText etJumlahKertas, etJumlahPlastik, etJumlahLogam, etJumlahKaca, etJumlahAlmu, etJumlahKardus;
+    EditText etNama, etAlamat, etNoHp;
+    Button btnLokasi, btnJual;
+
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jual_barang);
+
+        tvHargaKertas = findViewById(R.id.tvHargaKertas);
+        tvHargaPlastik = findViewById(R.id.tvHargaPlastik);
+        tvHargaLogam = findViewById(R.id.tvHargaLogam);
+        tvHargaKaca = findViewById(R.id.tvHargaKaca);
+        tvHargaAlmu = findViewById(R.id.tvHargaAlmu);
+        tvHargaKardus = findViewById(R.id.tvHargaKardus);
+
+        etJumlahKertas = findViewById(R.id.etJumlahKertas);
+        etJumlahPlastik = findViewById(R.id.etJumlahPlastik);
+        etJumlahLogam = findViewById(R.id.etJumlahLogam);
+        etJumlahKaca = findViewById(R.id.etJumlahKaca);
+        etJumlahAlmu = findViewById(R.id.etJumlahAlmu);
+        etJumlahKardus = findViewById(R.id.etJumlahKardus);
+
+        etNama = findViewById(R.id.etNama);
+        etAlamat = findViewById(R.id.etAlamat);
+        etNoHp = findViewById(R.id.etNoHp);
+
+        btnJual = findViewById(R.id.btnJual);
+        btnJual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JualBarang();
+            }
+        });
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(ActivityJualBarang.this);
+        btnLokasi = findViewById(R.id.btnLokasi);
+        btnLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DapatkanLokasi();
+            }
+        });
+
+    }
+
+    private void DapatkanLokasi() {
+        // GET CURRENT LOCATION
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    //initialize location
+                    Location location = task.getResult();
+                    if (location != null){
+                        try {
+                            //initialize geocoder
+                            Geocoder geocoder = new Geocoder(ActivityJualBarang.this, Locale.getDefault());
+
+                            //initialize address list
+                            List<Address> addresses = geocoder.getFromLocation(
+                                    location.getLatitude(),location.getLongitude(),1
+                            );
+                            Toast.makeText(ActivityJualBarang.this, "Lattide" + addresses.get(0).getLatitude(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(ActivityJualBarang.this, "Longitude" + addresses.get(0).getLongitude(), Toast.LENGTH_SHORT).show();
+                        }catch (IOException e){
+                            e.printStackTrace();
+
+
+                        }
+
+                    }
+                }
+            });
+        } else {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
+
+    }
+
+
+    private void JualBarang(){
+        //Mendapatkan UserID dari pengguna yang Terautentikasi
+        //String getUserID = auth.getCurrentUser().getUid();
+
+        //Mendapatkan Instance dari Database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference getReference;
+
+        //Menyimpan Data yang diinputkan User kedalam Variable
+        String getHargaKertas = tvHargaKertas.getText().toString();
+        String getHargaPlastik = tvHargaPlastik.getText().toString();
+        String getHargaLogam = tvHargaLogam.getText().toString();
+        String getHargaKaca = tvHargaKaca.getText().toString();
+        String getHargaAlmu = tvHargaAlmu.getText().toString();
+        String getHargaKardus = tvHargaKardus.getText().toString();
+
+        String getJumlahKertas = etJumlahKertas.getText().toString();
+        String getJumlahPlastik = etJumlahPlastik.getText().toString();
+        String getJumlahLogam = etJumlahLogam.getText().toString();
+        String getJumlahKaca = etJumlahKaca.getText().toString();
+        String getJumlahAlmu = etJumlahAlmu.getText().toString();
+        String getJumlahKardus = etJumlahKardus.getText().toString();
+
+        String getNama = etNama.getText().toString();
+        String getAlamat = etAlamat.getText().toString();
+        String getNoHp = etNoHp.getText().toString();
+
+
+        getReference = database.getReference(); // Mendapatkan Referensi dari Database
+/*
+        // Mengecek apakah ada data yang kosong
+        if(isEmpty(getNIM) && isEmpty(getNama) && isEmpty(getJurusan)){
+            //Jika Ada, maka akan menampilkan pesan singkan seperti berikut ini.
+            Toast.makeText(MainActivity.this, "Data tidak boleh ada yang kosong", Toast.LENGTH_SHORT).show();
+        }else {
+
+        Jika Tidak, maka data dapat diproses dan meyimpannya pada Database
+        Menyimpan data referensi pada Database berdasarkan User ID dari masing-masing Akun
+        */
+        getReference.child("Transaksi").push()
+                .setValue(new ModelJualBarang(getHargaKertas,getHargaPlastik,getHargaLogam,getHargaKaca,getHargaAlmu,getHargaKardus, getJumlahKertas,getJumlahPlastik,getJumlahLogam,getJumlahKaca,getJumlahAlmu,getJumlahKardus, getNama,getAlamat,getNoHp))
+                .addOnSuccessListener(this, new OnSuccessListener() {
+                    @Override
+                    public void onSuccess(Object o) {
+                        //Peristiwa ini terjadi saat user berhasil menyimpan datanya kedalam Database
+                        Toast.makeText(ActivityJualBarang.this, "Data Tersimpan", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
---
+++ app/src/main/java/com/example/recycle_app/Model/ModelJualBarang.java
@@ -0,0 +1,156 @@
+package com.example.recycle_app.Model;
+
+public class ModelJualBarang {
+    private String HargaKertas, HargaPlastik, HargaLogam, HargaKaca, HargaAlmu, HargaKardus, JumlahKertas, JumlahPlastik, JumlahLogam, JumlahKaca, JumlahAlmu, JumlahKerdus;
+    private String Nama, Alamat, NoHp;
+    private String Key;
+
+    public ModelJualBarang() {
+    }
+
+    public ModelJualBarang(String hargaKertas, String hargaPlastik, String hargaLogam, String hargaKaca, String hargaAlmu, String hargaKardus, String jumlahKertas, String jumlahPlastik, String jumlahLogam, String jumlahKaca, String jumlahAlmu, String jumlahKerdus, String nama, String alamat, String noHp) {
+        HargaKertas = hargaKertas;
+        HargaPlastik = hargaPlastik;
+        HargaLogam = hargaLogam;
+        HargaKaca = hargaKaca;
+        HargaAlmu = hargaAlmu;
+        HargaKardus = hargaKardus;
+        JumlahKertas = jumlahKertas;
+        JumlahPlastik = jumlahPlastik;
+        JumlahLogam = jumlahLogam;
+        JumlahKaca = jumlahKaca;
+        JumlahAlmu = jumlahAlmu;
+        JumlahKerdus = jumlahKerdus;
+        Nama = nama;
+        Alamat = alamat;
+        NoHp = noHp;
+    }
+
+    public String getHargaKertas() {
+        return HargaKertas;
+    }
+
+    public void setHargaKertas(String hargaKertas) {
+        HargaKertas = hargaKertas;
+    }
+
+    public String getHargaPlastik() {
+        return HargaPlastik;
+    }
+
+    public void setHargaPlastik(String hargaPlastik) {
+        HargaPlastik = hargaPlastik;
+    }
+
+    public String getHargaLogam() {
+        return HargaLogam;
+    }
+
+    public void setHargaLogam(String hargaLogam) {
+        HargaLogam = hargaLogam;
+    }
+
+    public String getHargaKaca() {
+        return HargaKaca;
+    }
+
+    public void setHargaKaca(String hargaKaca) {
+        HargaKaca = hargaKaca;
+    }
+
+    public String getHargaAlmu() {
+        return HargaAlmu;
+    }
+
+    public void setHargaAlmu(String hargaAlmu) {
+        HargaAlmu = hargaAlmu;
+    }
+
+    public String getHargaKardus() {
+        return HargaKardus;
+    }
+
+    public void setHargaKardus(String hargaKardus) {
+        HargaKardus = hargaKardus;
+    }
+
+    public String getJumlahKertas() {
+        return JumlahKertas;
+    }
+
+    public void setJumlahKertas(String jumlahKertas) {
+        JumlahKertas = jumlahKertas;
+    }
+
+    public String getJumlahPlastik() {
+        return JumlahPlastik;
+    }
+
+    public void setJumlahPlastik(String jumlahPlastik) {
+        JumlahPlastik = jumlahPlastik;
+    }
+
+    public String getJumlahLogam() {
+        return JumlahLogam;
+    }
+
+    public void setJumlahLogam(String jumlahLogam) {
+        JumlahLogam = jumlahLogam;
+    }
+
+    public String getJumlahKaca() {
+        return JumlahKaca;
+    }
+
+    public void setJumlahKaca(String jumlahKaca) {
+        JumlahKaca = jumlahKaca;
+    }
+
+    public String getJumlahAlmu() {
+        return JumlahAlmu;
+    }
+
+    public void setJumlahAlmu(String jumlahAlmu) {
+        JumlahAlmu = jumlahAlmu;
+    }
+
+    public String getJumlahKerdus() {
+        return JumlahKerdus;
+    }
+
+    public void setJumlahKerdus(String jumlahKerdus) {
+        JumlahKerdus = jumlahKerdus;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        Alamat = alamat;
+    }
+
+    public String getNoHp() {
+        return NoHp;
+    }
+
+    public void setNoHp(String noHp) {
+        NoHp = noHp;
+    }
+
+    public String getKey() {
+        return Key;
+    }
+
+    public void setKey(String key) {
+        Key = key;
+    }
+}
